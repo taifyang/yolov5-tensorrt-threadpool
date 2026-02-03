13c08359a629f32770ac7653f8b3f08ac86c16a5
taifyang <58515915+taifyang@users.noreply.github.com>
2026-02-03 22:31:14 +0800

    Update test_yolov5.cpp

--- test_yolov5.cpp
+++ test_yolov5.cpp
@@ -9,19 +9,32 @@ int main()
     std::vector<Detection> detections;
     yolov5->infer(image, detections);
     std::cout << "detections size: " << detections.size() << std::endl;
-    
-    clock_t start = clock();
-    for (int i = 0; i < 1; i++)
-    {
-        cv::Mat image = cv::imread("bus.jpg");
-        yolov5->infer(image, detections);
-    }
-    clock_t end = clock();
-    std::cout << "time: " << (double)(end - start) / CLOCKS_PER_SEC << "s" << std::endl;
-
     yolov5->draw_detections(0, image, detections);
     cv::imwrite("result.jpg", image);
+    
+    cv::VideoCapture cap("bj_full.mp4");
+    if (!cap.isOpened())
+        return -1;
+
+    int frame_count = 0;
+    auto start_all = std::chrono::high_resolution_clock::now();
+    while (true)
+    {
+        cap >> image;
+        if (image.empty())
+            break;
+        yolov5->infer(image, detections);
+        frame_count++;
+        auto end_all = std::chrono::high_resolution_clock::now();
+        auto elapsed_all = std::chrono::duration_cast<std::chrono::microseconds>(end_all - start_all).count() / 1000.f;
+        if (elapsed_all >= 1000)
+        {
+            printf("FPS:%f \n", frame_count / (elapsed_all / 1000.0f));
+            frame_count = 0;
+            start_all = std::chrono::high_resolution_clock::now();
+        }
+    }
+    cap.release();
 
     return 0;
 }
-
